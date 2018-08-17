88935130dca2b622dbd678b3990d5bfafc842021
coolsoftrf <coolsoft.rf@gmail.com>
2018-08-17 16:03:51 +0300

    extracted flash controller initialization into base activity

--- app/src/main/java/com/example/androidanimation/ActivityBase.java
+++ app/src/main/java/com/example/androidanimation/ActivityBase.java
@@ -2,17 +2,59 @@ package com.example.androidanimation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.androidanimation.flash.FlashController;
 
 abstract class ActivityBase extends AppCompatActivity {
     AniDroid droid;
     Button button;
 
+    FlashController mFlashController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mFlashController = new FlashController();
+
         setContentView(R.layout.activity_main);
         droid = findViewById(R.id.anidroid);
         button = findViewById(R.id.button);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mFlashController.checkPermissions(this)) {
+            mFlashController.init(this);
+            setupFlashButton();
+        } else {
+            mFlashController.requestPermissions(this);
+            Toast.makeText(this, "Please grant Camera permission", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFlashController.release();
+    }
+
+
+    public void setupFlashButton() {
+        if (mFlashController.isFlashSupported()) {
+            button.setVisibility(View.VISIBLE);
+
+            if (mFlashController.isTorchOn()) {
+                button.setText(R.string.flash_off);
+            } else {
+                button.setText(R.string.flash_on);
+            }
+        } else {
+            button.setVisibility(View.GONE);
+            Toast.makeText(this, "Flash is not supported.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/androidanimation/MainActivity.java
+++ app/src/main/java/com/example/androidanimation/MainActivity.java
@@ -1,38 +1,8 @@
 package com.example.androidanimation;
 
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-
-import com.example.androidanimation.flash.FlashController;
 
 public class MainActivity extends ActivityBase {
-
-    private FlashController mFlashController;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mFlashController = new FlashController();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (mFlashController.checkPermissions(this)) {
-            mFlashController.init(this);
-            setupFlashButton();
-        } else {
-            mFlashController.requestPermissions(this);
-            Toast.makeText(this, "Please grant Camera permission", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mFlashController.release();
-    }
 
     public void onFlash(View view) {
         if (mFlashController.isFlashSupported()) {
@@ -43,21 +13,6 @@ public class MainActivity extends ActivityBase {
                 mFlashController.turnFlashOn();
                 button.setText(R.string.flash_off);
             }
-        }
-    }
-
-    public void setupFlashButton() {
-        if (mFlashController.isFlashSupported()) {
-            button.setVisibility(View.VISIBLE);
-
-            if (mFlashController.isTorchOn()) {
-                button.setText(R.string.flash_off);
-            } else {
-                button.setText(R.string.flash_on);
-            }
-        } else {
-            button.setVisibility(View.GONE);
-            Toast.makeText(this, "Flash is not supported.", Toast.LENGTH_SHORT).show();
         }
     }
 }
